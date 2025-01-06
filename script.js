ef977bff4b97f51bb7c428a97311080db3e19cc0
m0rv1nd <168345833+m0rv1nd@users.noreply.github.com>
2025-01-06 10:24:46 +0100

    feat: add basic calc functions

---
+++ script.js
@@ -0,0 +1,41 @@
+let numberOne = 10;
+let numberTwo = 0;
+let operator = "/";
+
+function add (a, b) {
+    return a + b;
+}
+
+function subtract (a, b) {
+    return a - b;
+}
+
+function multiply (a, b) {
+    return a * b;
+}
+
+function divide (a, b) {
+    return a / b;
+}
+
+function operate (a, b, symbol) {
+    let result;
+
+    switch (symbol) {
+        case "+":
+            result = add (a, b);
+            break;
+        case "-":
+            result = subtract (a, b);
+            break;
+        case "*":
+            result = multiply (a, b);
+            break;
+        default:
+            result = divide (a, b);   
+    }
+    return result;
+}
+
+//console.log(operate(numberOne, numberTwo, operator));
+
