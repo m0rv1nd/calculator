4073d799ebd469e5bbccb8338ff485eb5d3d9b96
m0rv1nd <168345833+m0rv1nd@users.noreply.github.com>
2025-01-09 16:11:03 +0100

    basic TOP calculator done (without round numbers)

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
+/* round to avoid overflow 
+Math.round(123.4567 * 100) / 100; // 123.46
+let rounded = Number(num.toFixed(18));
+   
+   decimal dot
+   backspace button
+   keyboard support
+   cool layout 
+   color change
+   embellishment of error
+   
+   DONE:
+   clear button
+   divide by 0
+   first negative number
+*/
+
 const digitButtons = document.querySelectorAll(".digits");
 const operatorButtons = document.querySelectorAll(".operator");
-const equalButton = document.querySelector(".equal");
-const input = document.querySelector("input");
+const equalButtons = document.querySelectorAll(".equal");
+const inputResult = document.querySelector("#result");
+const inputCalc = document.querySelector("#calculation");
+const clearButton = document.querySelector("#clear");
+const negativeButton = document.querySelector(".negative");
 
 let numberOne = "";
 let numberTwo = "";
 let operator = "";
-input.value = 0;
+inputResult.value = 0;
 
 function add (a, b) {
     return a + b;
@@ -21,7 +41,8 @@ function multiply (a, b) {
 }
 
 function divide (a, b) {
-    return a / b;
+    if (b != 0) return a / b;
+    else return "ERROR";
 }
 
 function operate (a, b, symbol) {
@@ -46,26 +67,94 @@ function operate (a, b, symbol) {
 digitButtons.forEach(button => {
     button.addEventListener("click", () => {
         if (!operator) {
-            numberOne += button.value;
-            input.value = numberOne;
+            if (inputResult.value != "ERROR") {
+                numberOne += button.value;
+                inputResult.value = numberOne;
+            } else {
+                numberOne = "";
+                inputResult.value = "";
+                numberOne += button.value;
+                inputResult.value = numberOne;
+            }
         } else {
             numberTwo += button.value;
-            input.value = numberOne + operator + numberTwo;
-        }
+            inputResult.value = numberOne + operator + numberTwo;
+        } 
     });
 });
 
 operatorButtons.forEach(button => {
     button.addEventListener("click", () => {
-        if (numberOne && !numberTwo) {
+        if (numberOne && numberOne != "-" && !numberTwo && !operator && inputResult.value !="ERROR") {
             operator = button.value;
-            input.value = numberOne + operator;
-        }
+            inputResult.value = numberOne + operator;
+        } else if (inputResult.value == "ERROR") {
+            operator = "";
+        } 
     });
 });
 
-equalButton.addEventListener("click", () => {
-    if (numberOne != "" && numberTwo != "" && operator != "") {
-        alert(operate(numberOne, numberTwo, operator));
+equalButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        if (numberOne != "" && numberTwo != "" && operator != "") {
+            switch (button.value) {
+                case "=":
+                    inputResult.value = operate(+numberOne, +numberTwo, operator);
+                    inputCalc.value = numberOne + operator + numberTwo;
+                    numberOne = inputResult.value;
+                    operator = "";
+                    numberTwo = "";
+                    break;
+                case "+":
+                    inputResult.value = operate(+numberOne, +numberTwo, operator);
+                    inputCalc.value = numberOne + operator + numberTwo;
+                    numberOne = inputResult.value;
+                    operator = "+";
+                    numberTwo = "";
+                    inputResult.value = numberOne + operator;
+                    break;
+                case "-":
+                    inputResult.value = operate(+numberOne, +numberTwo, operator);
+                    inputCalc.value = numberOne + operator + numberTwo;
+                    numberOne = inputResult.value;
+                    operator = "-";
+                    numberTwo = "";
+                    inputResult.value = numberOne + operator;
+                    break;
+                case "*":
+                    inputResult.value = operate(+numberOne, +numberTwo, operator);
+                    inputCalc.value = numberOne + operator + numberTwo;
+                    numberOne = inputResult.value;
+                    operator = "*";
+                    numberTwo = "";
+                    inputResult.value = numberOne + operator;
+                    break;
+                case "/":
+                    inputResult.value = operate(+numberOne, +numberTwo, operator);
+                    inputCalc.value = numberOne + operator + numberTwo;
+                    numberOne = inputResult.value;
+                    operator = "/";
+                    numberTwo = "";
+                    inputResult.value = numberOne + operator;
+                    break;
+            }
+        } 
+    });
+});
+
+clearButton.addEventListener("click", () => {
+    numberOne = "";
+    numberTwo = "";
+    operator = "";
+    inputResult.value = 0;
+    inputCalc.value = "";
+});
+
+negativeButton.addEventListener("click", () => {
+    if (!numberOne) {
+        numberOne = negativeButton.value;
+        inputResult.value = numberOne;
     }
 });
+
+
